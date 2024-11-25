a97d357bbd24f6e0da50493d32ad52d1298d0ac0
Collin Maillet <cmaillet@lcs.on.ca>
2024-11-25 11:10:41 -0500

    Initial Commit

---
+++ FavThings/FavThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavThingsApp.swift
+//  FavThings
+//
+//  Created by Collin Maillet on 2024-11-25.
+//
+
+import SwiftUI
+
+@main
+struct FavThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
